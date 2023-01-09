e251c860d7b507944de4d6e2ebb528892c869276
Lan Pavletic <pavletic.lan@gmail.com>
2023-01-09 15:27:06 +0100

    removed email filed in securety modal and added cancel option

--- lib/screens/mainScreens/profileScreen/modals/securitySettingsModal.dart
+++ lib/screens/mainScreens/profileScreen/modals/securitySettingsModal.dart
@@ -1,11 +1,7 @@
-import 'package:dash/helpers/colors.dart';
-import 'package:dash/screens/mainScreens/profileScreen/modals/accountSettingsModal.dart';
-import 'package:dash/screens/mainScreens/profileScreen/modals/profileSettingsModal.dart';
-import 'package:dash/screens/mainScreens/profileScreen/modals/securitySettingsModal.dart';
-import 'package:dash/screens/widgets/settingsButton.dart';
 import 'package:dash/state/firebaseState/firebaseState.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class SecuritySettingsModal extends ConsumerWidget {
   const SecuritySettingsModal({Key? key}) : super(key: key);
@@ -13,8 +9,6 @@ class SecuritySettingsModal extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final auth = ref.read(authenticationProvider);
-    final screenWidth = MediaQuery.of(context).size.width;
-    final emailController = TextEditingController();
     //Bottom modal sheet appbar with cancel, title and save button
     return Column(
       children: [
@@ -24,7 +18,7 @@ class SecuritySettingsModal extends ConsumerWidget {
           children: [
             TextButton(
                 onPressed: () {
-                  // context.pop()
+                  context.pop();
                 },
                 child: const Text('Cancel')),
             const Text(
@@ -46,24 +40,7 @@ class SecuritySettingsModal extends ConsumerWidget {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            const Text(""),
-            const SizedBox(height: 10),
-            //Field for name
-            SizedBox(
-              width: screenWidth * 0.8,
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email address',
-                  hintText: 'Enter your email for the recovery password',
-                  hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
-                ),
-              ),
-            ),
-          ],
+          children: [],
         )
       ],
     );
